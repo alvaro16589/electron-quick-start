5c84edf70f0c9a851036825ee840d2df50bb31fa
alvaro <54456217+alvaro16589@users.noreply.github.com>
2023-05-03 15:20:49 -0400

    last chage

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    titleBarStyle: 'hidden',
+    titleBarOverlay: true,
+    width: 1200,
+    height: 700,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('optica_gala_store/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
